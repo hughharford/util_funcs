36ec1a050183a70c36a223c346acc85a39a5efc6
hughharford <hugh.harford@poscoconsulting.com>
2022-07-08 10:45:06 +0100

    getting into logistic modelling with l1 and l2 pentalities

--- util_funcs/ML_refs/ml_101.py
+++ util_funcs/ML_refs/ml_101.py
@@ -8,6 +8,7 @@
 # %autoreload 2
 
 # import fundamentals
+# import visualisation tools
 # import preprocessing
 # import scikit-learning modelling
 # import model assessment
@@ -17,10 +18,12 @@
 import math
 import pandas as pd
 import numpy as np
+import pickle
+
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 import seaborn as sns
-import pickle
+import plotly.graph_objects as go
 
 # preprocessing
 from sklearn.impute import SimpleImputer
@@ -99,15 +102,30 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Masking
 
+# ############################## ###############################
+#       Loading data
+# ############################## ###############################
+#
+#
+#
+#
+#
+#
+#
 
-
-# Load raw data
 data = pd.read_csv('path_to_csv_file.csv')
 
 
 # ############################## ###############################
 #       Data Exploratory Data Analysis
 # ############################## ###############################
+#
+#
+#
+#
+#
+#
+#
 
 data.shape
 data.head(15)
@@ -118,6 +136,13 @@ data.info()
 # ############################## ###############################
 #       Data Visualisation
 # ############################## ###############################
+#
+#
+#
+#
+#
+#
+#
 
 data.boxplot();
 sns.scatterplot(data=data, x='column_name_x', y='column_name_y');
@@ -127,9 +152,29 @@ plt.xlabel('a')
 plt.ylabel('b')
 plt.scatter(data['column_a'], data['column_b'], color = 'red');
 
+# very cool 3d, interactive plot
+import plotly.graph_objects as go
+# for use in a Jupyter notebook...?
+Z, range_a, range_b = 50,50,50
+surface = go.Surface(x=range_a, y=range_b, z=Z)
+scatter = go.Scatter3d(x=data['column_a'], y=data['column_b'],
+                       z=data['loss_history'], mode='markers')
+fig = go.Figure(data=[surface, scatter])
+
+fig.update_layout(title='Loss Function', autosize=False,
+                  width=1000, height=800)
+fig.show()
+
 # ############################### ###############################
 #       Data selection
 # ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
 
 # Only keep numerical columns and raws without NaN
 data = data.select_dtypes(include=np.number).dropna()
@@ -138,6 +183,13 @@ data = data.select_dtypes(include=np.number).dropna()
 # ############################### ###############################
 #       Test / Train split
 # ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
 
 y = data['column_name']
 X = data.drop(columns = 'column_name')
@@ -148,8 +200,94 @@ X_train.shape
 # ############################### ###############################
 #           Scaling
 # ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
 
 # most simple scaling possible
 std_scaler = StandardScaler()
 X_scaled_train = std_scaler.fit_transform(X_train)
 X_scaled_t = pd.DataFrame(X_scaled_train)
+
+# MinMaxScale our features for you
+scaler = MinMaxScaler().fit(X)
+X_scaled = scaler.transform(X)
+X_scaled = pd.DataFrame(X_scaled, columns=X.columns)
+X.shape
+
+# ############################### ###############################
+#           Regularisation
+# ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+# ############################### ###############################
+#           Modelling - Linear Regression
+# ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
+
+#           Modelling - Linear Regression
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# 1. basic Logistic Regression Model
+
+log_model = LogisticRegression(max_iter = 1000)
+log_model = log_model.fit(X_scaled, y)
+cv_results = cross_validate(log_model, X_scaled, y, cv=5)
+cv_results
+
+# see feature names and their coefficients
+lister = {}
+for index, elem in enumerate(log_model.feature_names_in_):
+    lister[log_model.feature_names_in_[index]] = log_model.coef_[0][index]
+lister
+
+# TODO: Classification and logistic regression: Revise log-odds
+
+# 2.  Logistic Regression with a L2 penalty
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Let's use a **Logistic model** whose log-loss has been penalized with a
+# **L2** term to figure out the **most important features** without
+# overfitting.
+# This is the "classification" equivalent to the "Ridge" regressor
+
+# - By "strongly regularized" we mean "more than sklearn's default applied
+#  regularization factor".
+# - Default sklearn's values are very useful orders of magnitudes to keep
+# in mind for "scaled features"
+
+l1_ratio = 0 # 0 == L2 penalty
+# just the l1_ratio did, but threw warnings
+# set C low to emulate alpha
+C = 0.001
+strong_l2_log_model = LogisticRegression(C=C)
+strong_l2_log_model.fit(X_scaled, y)
+l2_cv_results = cross_validate(strong_l2_log_model, X_scaled, y, cv=5)
+l2_cv_results
+
+l2_lister = {}
+for index, elem in enumerate(strong_l2_log_model.feature_names_in_):
+    l2_lister[strong_l2_log_model.feature_names_in_[index]] = strong_l2_log_model.coef_[0][index]
+l2_lister
+
+# sort results
+df_scores = pd.DataFrame(data=strong_l2_log_model.coef_[0], index=strong_l2_log_model.feature_names_in_)
+abs(df_scores[0]).sort_values(ascending=True)
