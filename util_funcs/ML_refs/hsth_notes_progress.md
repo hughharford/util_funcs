586518fa291a97248d983fb3bf417f941fa58486
hughharford <hugh.harford@poscoconsulting.com>
2022-07-08 11:52:15 +0100

    progress on ml notes

---
+++ util_funcs/ML_refs/hsth_notes_progress.md
@@ -0,0 +1,14 @@
+
+
+### underway:
+
+## 02 Prepare the dataset
+
+
+## 05 Model Tuning
+- done:
+  - 01-Workflow
+  - 02-Regularisation
+- didn't get to:
+  - 03 and onwards
+  -
--- util_funcs/ML_refs/ml_101.py
+++ util_funcs/ML_refs/ml_101.py
@@ -16,6 +16,7 @@
 # import pipelines
 
 import math
+import random
 import pandas as pd
 import numpy as np
 import pickle
@@ -57,6 +58,8 @@ from sklearn.inspection import permutation_importance
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from scipy import stats
 
 # sklearn metrics
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, max_error
@@ -133,6 +136,31 @@ data.tail()
 data.describe() # min, max, mean, count etc
 data.info()
 
+data.columns # see column names
+
+data[['column_name']].value_counts()
+
+#       Duplicates
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# count duplicates
+duplicate_count = data.duplicated().sum()
+duplicate_count
+
+# drop all duplicates
+data = data.drop_duplicates()
+
+
+#       Missing data
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# list empty values as a percentage by feature
+empty = data.isnull().sum().sort_values(ascending=False);
+empty / len(data)
+
+#       Replacing values
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data.FeatureName.replace(np.nan, "NoG", inplace=True) #Replace NaN with...
+
 # ############################## ###############################
 #       Data Visualisation
 # ############################## ###############################
@@ -144,12 +172,23 @@ data.info()
 #
 #
 
-data.boxplot();
-sns.scatterplot(data=data, x='column_name_x', y='column_name_y');
+# simple plots
 plt.plot(some_data_or_df = data);
 
+# chart features
 plt.xlabel('a')
 plt.ylabel('b')
+
+
+# boxplot
+data.boxplot(); # good for seeing outliers
+
+# histograms
+data.hist('feature_a'); # pd histogram
+sns.histplot(data=data,x="feature",bins=30);
+
+# scatter plots
+sns.scatterplot(data=data, x='column_name_x', y='column_name_y');
 plt.scatter(data['column_a'], data['column_b'], color = 'red');
 
 # very cool 3d, interactive plot
@@ -176,9 +215,14 @@ fig.show()
 #
 #
 
+# drop columns
+data = data.drop(columns = 'feature')
+
 # Only keep numerical columns and raws without NaN
 data = data.select_dtypes(include=np.number).dropna()
 
+# select by column name
+data = data[['column_one','column_two']]
 
 # ############################### ###############################
 #       Test / Train split
@@ -198,6 +242,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 X_train.shape
 
 # ############################### ###############################
+#           Imputing
+# ############################### ###############################
+
+data[['feature_a']].boxplot(); # plot before
+imputer = SimpleImputer(strategy="mean")
+imputer.fit(data[['feature_a']])
+data['feature_a'] = imputer.transform(data[['feature_a']])
+data[['feature_a']].boxplot(); # plot after
+
+
+# ############################### ###############################
 #           Scaling
 # ############################### ###############################
 #
@@ -207,6 +262,14 @@ X_train.shape
 #
 #
 #
+
+# TODO: Cover scaler and when you use each one
+
+# Scaler                    Use
+# ==========================================================
+# StandardScaler
+# MixMaxScaler
+# RobustScaler
 
 # most simple scaling possible
 std_scaler = StandardScaler()
@@ -218,6 +281,10 @@ scaler = MinMaxScaler().fit(X)
 X_scaled = scaler.transform(X)
 X_scaled = pd.DataFrame(X_scaled, columns=X.columns)
 X.shape
+
+r_scaler = RobustScaler() # Instanciate Robust Scaler
+r_scaler.fit(data[['feature']]) # Fit scaler to feature
+data['feature'] = r_scaler.transform(data[['feature']]) # apply scale
 
 # ############################### ###############################
 #           Regularisation
@@ -248,6 +315,7 @@ X.shape
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 # 1. basic Logistic Regression Model
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 log_model = LogisticRegression(max_iter = 1000)
 log_model = log_model.fit(X_scaled, y)
@@ -264,6 +332,7 @@ lister
 
 # 2.  Logistic Regression with a L2 penalty
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 # Let's use a **Logistic model** whose log-loss has been penalized with a
 # **L2** term to figure out the **most important features** without
 # overfitting.
@@ -285,9 +354,130 @@ l2_cv_results
 
 l2_lister = {}
 for index, elem in enumerate(strong_l2_log_model.feature_names_in_):
-    l2_lister[strong_l2_log_model.feature_names_in_[index]] = strong_l2_log_model.coef_[0][index]
+    l2_lister[strong_l2_log_model.feature_names_in_[index]] = \
+        strong_l2_log_model.coef_[0][index]
 l2_lister
 
-# sort results
-df_scores = pd.DataFrame(data=strong_l2_log_model.coef_[0], index=strong_l2_log_model.feature_names_in_)
+# sort and view results
+df_scores = pd.DataFrame(data=strong_l2_log_model.coef_[0],
+                         index=strong_l2_log_model.feature_names_in_)
 abs(df_scores[0]).sort_values(ascending=True)
+
+# 3. Logistic Regression with a L1 penalty
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# A logistic model whose log-loss has been penalized
+# with a **L1** term to **filter-out the less important features**.
+# This is the "classification" equivalent to the **Lasso** regressor
+
+C=0.2
+strong_l1_log_model = LogisticRegression(solver='liblinear',
+                                         C=C, penalty='l1', max_iter=1000)
+strong_l1_log_model.fit(X_scaled, y)
+l1_cv_results = cross_validate(strong_l1_log_model, X_scaled, y, cv=5)
+
+
+# ############################### ###############################
+#           GridSearch, RandomSearch
+# ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
+
+# manually done first using Cross Validation
+# Plot the scores as a function of K to visually find the best
+# K value using the `Elbow Method`
+manual = []
+for k in range(1,50,1):
+    knn_model = KNeighborsRegressor(n_neighbors=k)
+    cv_results = cross_val_score(knn_model, X_scaled_t, y_train, cv=5)
+    manual.append(cv_results.mean())
+
+plt.plot(manual);
+
+# GridSearch with multiple parameters
+k = [1, 5, 10, 20, 50]
+p = [1, 2, 3]
+
+# Instanciate model
+knn_model = KNeighborsRegressor()
+
+# Hyperparameter Grid
+grid = {
+    'n_neighbors': k,
+    'p': p
+        }
+
+# Instanciate Grid Search
+search = GridSearchCV(
+                knn_model,
+                grid,
+                scoring = 'r2',
+                cv=5,
+                n_jobs=-1
+                )
+
+# Fit data to Grid Search
+search.fit(X_scaled_t, y_train)
+
+print(f'best_score_ {search.best_score_}')
+print(f'best_params_ {search.best_params_}')
+print(f'best_estimator_ {search.best_estimator_}')
+
+
+k = [random.randint(1,50) for x in range(20)]
+p = [1, 2, 3]
+
+# Instanciate model
+knn_model = KNeighborsRegressor()
+
+# Hyperparameter Grid
+grid = {
+    'n_neighbors': k,
+    'p': p
+        }
+
+
+# Instanciate Randomised Search
+search = RandomizedSearchCV(
+                knn_model,
+                grid,
+                scoring = 'r2',
+                cv=5,
+                n_iter=15,
+                n_jobs=-1
+                )
+
+# Fit data to Randomised Search
+search.fit(X_scaled_t, y_train)
+
+print(f'best_score_ {search.best_score_}')
+print(f'best_params_ {search.best_params_}')
+print(f'best_estimator_ {search.best_estimator_}')
+
+best_p = search.best_params_['p']
+best_n_neighbors = search.best_params_['n_neighbors']
+best_model = KNeighborsRegressor(n_neighbors=best_n_neighbors, p=best_p)
+
+best_model = best_model.fit(X_scaled_t, y_train)
+
+# ############################### ###############################
+#           Predict
+# ############################### ###############################
+#
+#
+#
+#
+#
+#
+#
+
+# with best model from RandomisedSearch above
+X_scaled_test = std_scaler.transform(X_test)
+y_pred = best_model.predict(X_scaled_test)
+r2_test = r2_score(y_test, y_pred)
+r2_test
